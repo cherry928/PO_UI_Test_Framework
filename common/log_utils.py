d519f0d1195ce9219a013a2ec20d86bf81fdad1c
cherry928 <15073140749@163.com>
2020-04-19 15:31:30 +0800

    第二次提交： 1、编写main_page模块熟练po操作 2、通过main_page模块 反思发现页面类有许多相同相似的属性和方法 能够构建一个父类？ 3、引入日志模块，并发现使用的时候不方便，每个页面上的控件操作都需要写一条日志 4、日志模块解决重复：在log_utils.py中，直接定义日志对象
    版本：1.0.1

---
+++ common/log_utils.py
@@ -0,0 +1,23 @@
+import os
+import logging
+
+current_path = os.path.dirname(__file__)
+log_path = os.path.join(current_path, '../logs/test.log')
+
+class LogUtils:
+    def __init__(self, logfile_path=log_path):
+        self.__logfile_path = logfile_path
+        self.logger = logging.getLogger()
+        self.logger.setLevel(level=logging.INFO)
+        file_log = logging.FileHandler(self.__logfile_path)
+        formatter = logging.Formatter('file:%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_log.setFormatter(formatter)
+        self.logger.addHandler(file_log)
+    def info(self, message):
+        self.logger.info(message)
+    def error(self, message):
+        self.logger.error(message)
+logger = LogUtils()
+if __name__=='__main__':
+    log_utils = LogUtils()
+    log_utils.info('newdream')
